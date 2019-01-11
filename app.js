8e751e15c7af51d68cef8cdbe7f05d8aef706387
SummerLi <xl1029@nyu.edu>
2019-01-11 10:54:06 +0800

    Feat
    Setup starting html

--- app.js
+++ app.js
@@ -5,5 +5,18 @@ const io = require('socket.io')(server);
 app.use(express.static('public'));
 
 
+const racers = {'racer1':0, 'racer2':0};
+io.on('connection', (socket) => {
+ 
+    io.emit('click', racers);
+ 
+    console.log(socket.id, 'connected');
+    socket.on('click', (data) => {
+        racers[data] += 5;
+        io.emit('click', racers);
+        console.log(data);
+    });
+});
 
-server.listen(3000);
+// server looks in the env for the port number!
+server.listen(process.env.PORT);
